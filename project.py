30560a374c39ae195a0a1c2fade7b631607fe8ca
Ping Tseng <yt376@cornell.edu>
2017-12-21 18:21:43 -0500

    project ideas

---
+++ project.py
@@ -0,0 +1,13 @@
+#Python project ideas
+#GAMES
+######
+# Battleship
+# Tetris
+# Interactive Text-Based game
+#
+# OTHER IDEAS
+##############
+# Make a text editor
+# Make an event planning calendar using GUI (perhaps HTML also)
+# Search engine
+# Travel Itinerary Planner
